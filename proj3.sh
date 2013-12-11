d51c149747aeee2dda6387b8034de4bbadb6a499
Patrick Lewis <patl1@vt.edu>
2013-12-11 16:17:55 -0500

    Fixed script, updated part a source file

--- proj3.sh
+++ proj3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #=========================Part A====================================#
+mkdir -p results
 n1=512
 n2=1024
 n3=4096
@@ -9,37 +10,41 @@ declare -a cores=(1 2 4 8 16 32)
 declare -a sizes=(512 724 1024 1448 2048 2912)
 
 #compile and then check for errors
-mpicc -o part_a -g -Wall -Werror ring_matrix_part_a.c
-if [ $? -ne 0 ]; then
-    return
-fi
+#mpicc -o part_a -g -Wall -Werror ring_matrix_part_a.c
+#if [ $? -ne 0 ]; then
+#    return
+#fi
 
 #set up data files
-printf "np time speedup" > n1_results_part_a
-printf "np time speedup" > n2_results_part_a
-printf "np time speedup" > n3_results_part_a
-printf "np size time flops mflops/s" > isogranularity_part_a
+printf "np time speedup" > results/n1_results_part_a
+printf "np time speedup" > results/n2_results_part_a
+printf "np time speedup" > results/n3_results_part_a
+printf "np size time flops mflops/s" > results/isogranularity_part_a
 
 # Do data run for speedup numbers
-for n in n1 n2 n3
+printf "Collecting data\n"
+for n in $n1 $n2 $n3
 do
-    for p in cores   #different processor numbers
+    printf "%d\n" $n #for debug
+    for p in "${cores[@]}"   #different processor numbers
     do
+        printf "%d" $p #for debug
         for i in {1..100}       #run multiple times for good data
         do
             mpirun -np $p -hostfile nodes ./part_a $n >> tmp
+            printf "." #for debug
         done
         #if this is the first one, remember the time for speedup calculations
-        if [ $p -eq 1 ]; then
+        if [ "$p" -eq 1 ]; then
             spdf=$(awk '{sum+=$1} END {avg=sum/NR; print avg}' tmp)
         fi
         #do speedup calculations, send to correct files
-        if [ $n -eq $n1 ]; then
-            awk -v fac="$spdf" -v procs="$p" '{sum+=$1} END {avg=sum/NR; print procs,avg,avg/fac}' tmp >> n1_results_part_a 
-        elif [ $n -eq $n2 ]; then
-            awk -v fac="$spdf" -v procs="$p" '{sum+=$1} END {avg=sum/NR; print procs,avg,avg/fac}' tmp >> n2_results_part_a 
-        elif [ $n -eq $n3 ]; then
-            awk -v fac="$spdf" -v procs="$p" '{sum+=$1} END {avg=sum/NR; print procs,avg,avg/fac}' tmp >> n3_results_part_a 
+        if [ "$n" -eq "$n1" ]; then
+            awk -v fac="$spdf" -v procs="$p" '{sum+=$1} END {avg=sum/NR; print procs,avg,avg/fac}' tmp >> results/n1_results_part_a 
+        elif [ "$n" -eq "$n2" ]; then
+            awk -v fac="$spdf" -v procs="$p" '{sum+=$1} END {avg=sum/NR; print procs,avg,avg/fac}' tmp >> results/n2_results_part_a 
+        elif [ "$n" -eq "$n3" ]; then
+            awk -v fac="$spdf" -v procs="$p" '{sum+=$1} END {avg=sum/NR; print procs,avg,avg/fac}' tmp >> results/n3_results_part_a 
         fi
         #remove temp file for the next iteration
         rm -f tmp
@@ -47,17 +52,22 @@ do
 done
 
 # Do data runs for isogranularity
-for k in {1..${#cores}}
+printf "Collecting data for isogranularity\n"
+numcores=${#cores[@]}
+for k in {0..5}
 do
+    printf "%d cores" ${cores[$k]}
     for i in {1..100}
     do
+	printf "."
         mpirun -np ${cores[$k]} -hostfile nodes ./part_a ${sizes[$k]} >> tmp
     done
     #do math and come up with FLOPS numbers
-   awk -v procs="${cores[$k]}" -v size="${sizes[$k]}" '{sum+=$1} END {avg=sum/NR; flops=2*size^3; print procs,size,avg,flops,(flops/1000)/avg}' tmp >> isogranularity_part_a
+    awk -v procs="${cores[$k]}" -v size="${sizes[$k]}" '{sum+=$1} END {avg=sum/NR; flops=2*size^3; print procs,size,avg,flops,(flops/1000)/avg}' tmp >> results/isogranularity_part_a
    rm -f tmp
 done 
 
+printf "Making plots\n"
 # Make a plot of speedup
 cat << __EOF | gnuplot
 set term png size 800,600 font "/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf"
@@ -66,15 +76,13 @@ set title "Speedup vs. Number of Processes"
 set xlabel "Processes (p)"
 set ylabel "Speedup Factor"
 set autoscale
-plot "n1_results_part_a" using 1:3 title "n = ${n1}" with linespoints pointtype 6 lw 5
-plot "n2_results_part_a" using 1:3 title "n = ${n2}" with linespoints pointtype 6 lw 5
-plot "n3_results_part_a" using 1:3 title "n = ${n3}" with linespoints pointtype 6 lw 5
+plot "results/n1_results_part_a" using 1:3 title "n = ${n1}" with linespoints pointtype 6 lw 5, "results/n2_results_part_a" using 1:3 title "n = ${n2}" with linespoints pointtype 6 lw 5, "results/n3_results_part_a" using 1:3 title "n = ${n3}" with linespoints pointtype 6 lw 5
 
-set term postscript eps size 8.6 font "/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf, 30" enhanced color
+set term postscript eps size 8,6 font "/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf, 30" enhanced color
 set output "speedup_plot_part_a.eps"
-plot "n1_results_part_a" using 1:3 title "n = ${n1}" with linespoints pointtype 6 lw 10
-plot "n2_results_part_a" using 1:3 title "n = ${n2}" with linespoints pointtype 6 lw 10
-plot "n3_results_part_a" using 1:3 title "n = ${n3}" with linespoints pointtype 6 lw 10
+plot "results/n1_results_part_a" using 1:3 title "n = ${n1}" with linespoints pointtype 6 lw 10
+plot "results/n2_results_part_a" using 1:3 title "n = ${n2}" with linespoints pointtype 6 lw 10
+plot "results/n3_results_part_a" using 1:3 title "n = ${n3}" with linespoints pointtype 6 lw 10
 __EOF
 
 # Make the isogranularity plot
@@ -83,16 +91,16 @@ set term png size 800,600 font "/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf"
 set output "isogranularity_part_a.png"
 set title "Isogranularity"
 set xlabel "Number of Processes (p)"
-set ylabel "Millions of Floating-Point Operations per Second (mFLOPS/s):
+set ylabel "Millions of Floating-Point Operations per Second (mFLOPS/s)"
 set autoscale
-plot "isogranularity_part_a" using 1:4 with linespoints pointtype 6 lw 5
+plot "results/isogranularity_part_a" using 1:5 title "iso" with linespoints pointtype 6 lw 5
 
-set term postscript eps size 8.6 font "/usr/share/fonts/dejavu/DejavuSans-Bold.ttf, 30" enhanced color
+set term postscript eps size 8,6 font "/usr/share/fonts/dejavu/DejavuSans-Bold.ttf, 30" enhanced color
 set output "isogranularity_part_a.eps"
-plot "isogranularity_part_a" using 1:4 with linespoints pointtype 6 lw 10
+plot "results/isogranularity_part_a" using 1:5 title "iso" with linespoints pointtype 6 lw 10
 __EOF
 
-
+mv *.png *.eps resutls/
 
 #=========================Part B====================================#
 #=========================Part C====================================#
--- ring_matrix_part_a.c
+++ ring_matrix_part_a.c
@@ -9,18 +9,20 @@ char* usage = "Usage: ./a.out <num_procs>\n where <num_procs> is the number of p
 int main(int argc, char* argv[])
 {
     /* Preprocessor Definitions */
-#define amat(I,J) A[I + n*J]
+#define amat(I,J) A[I + n*J] 
 #define bmat(I,J) B[I + n*J]
 #define cmat(I,J) C[I + n*J]
 //#define DEBUG 1
 
     /* Variables */
-    int     num_procs;              //total number of processors    
-    int     proc_num;               //this processor's rank
-    int     n;                      //size of the matrix we will use
-    double  start, finish;          //used for timing matrix computations
-    char    hostname[20];           //hostname of this machine (for debug)
-    double  *A, *B, *C;             //the matrices we'll use
+    int     p;                          //total number of processors    
+    int     proc_num;                   //this processor's rank
+    int     n;                          //size of the matrix we will use
+    int     nc;                         //the "width" of each matrix column"
+    int     i,j,k;                      //counter variables
+    double  start, finish;              //used for timing matrix computations
+    char    hostname[20];               //hostname of this machine (for debug)
+    double  *A, *B, *C, *Atemp;         //the matrices we'll use
 
     /* Initializations */
     MPI_Init(NULL, NULL);
@@ -29,7 +31,7 @@ int main(int argc, char* argv[])
     hostname[19] = '\0';
     gethostname(hostname, 19);
 #   ifdef DEBUG
-    printf("Process %d of %d running on host %s\n", proc_num, num_procs, hostname);
+    printf("Process %d of %d running on host %s\n", proc_num, p, hostname);
 #   endif
     if(argc == 1)           n = 1024;
     else if (argc == 2)     n = atoi(argv[1]);
@@ -39,21 +41,21 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    A = (double *)malloc(n * n * sizeof(double));
-    B = (double *)malloc(n * n * sizeof(double));
-    C = (double *)malloc(n * n * sizeof(double));
-
-    int i;
+    A     = (double *)malloc(n * nc * sizeof(double));
+    Atemp = (double *)malloc(n * nc * sizeof(double));
+    B     = (double *)malloc(n * nc * sizeof(double));
+    C     = (double *)malloc(n * nc * sizeof(double));
+     
     srand(0);
-    for(i = 0; i < n*n; i++)
-    {
-        A[i] = rand();
-        B[i] = rand();
-    }
 
+    /* 
+     * TODO:
+     * Have root process intialize all the data and then
+     * send it out to the worker processes
+     */
 
-    /* Matrix Calculations */
-    GET_TIME(start);
+    k = proc_num; //just to follow the pseudocode easier
+
     //pseudocode
     //Ck = Ck + Ak*Bkk
     //Atemp = Ak
@@ -64,10 +66,55 @@ int main(int argc, char* argv[])
     //  receive Atemp from right
     //  Ck = Ck + Atemp*Bjk
     //end
+
+    /* Matrix Calculations */
+    MPI_Barrier(MPI_COMM_WORLD); //do barrier so that we synchronize for better time
+    GET_TIME(start);
     
+    //real code
+    calc_c(k,k);
+    Atemp = Ak;
+    j = k;
+    for(i = 1; i < p-1; i++)
+    {
+        j = mod(j+1, p);
+        
+        //send left
+        if(k > 0)
+        {
+            MPI_Send(Atemp, nc, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        }
+        //send right
+        if(k < p-1)
+        {
+            MPI_Receive(Atemp, nc, MPI_DOUBLE,k+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        calc_c(j,k); 
+    }
+    MPI_Barrier(MPI_COMM_WORLD);    
     GET_TIME(finish);
-    printf("%f\n", hostname, finish-start);
+    printf("%f\n", finish-start);
 
     MPI_Finalize();
     return 0;
 } /* main */
+
+/* Used to avoid having really messy code.  */
+void calc_c(int j2, int k2)
+{
+    //Ck = Ck + Ak*Bkk
+    int sum;
+    int x,y,z;
+    for (x=0; x<n; x++) 
+    {
+        for (y=0; y<nc; y++) 
+        {
+            sum = cmat(x,y);
+            for (z=0; z<nc; z++) 
+            {
+                sum = sum + amat(x,k2+y) * bmat(j2+z,k2+y);
+            }
+            cmat(x,y) = sum;
+        }
+    }
+}
